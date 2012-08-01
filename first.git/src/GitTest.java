b342f2778d792472d7b17adce64cca25d465eca7
Sony <Sony@Sony-PC>
2012-08-01 16:19:25 +0530

    Revert "Small change from ans to que"
    This reverts commit 9c5495be8275bb3a290107454cb21157d17902ee.

--- first.git/src/GitTest.java
+++ first.git/src/GitTest.java
@@ -6,7 +6,7 @@ public class GitTest {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Is Git is Fun?");
+		System.out.println("Git is Fun");
 	}
 
 }
